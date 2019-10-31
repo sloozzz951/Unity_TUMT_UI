f66b8e97a4fd4fabab6ec8b87b35d57ddf8229e4
sloozzz951 <55227248+sloozzz951@users.noreply.github.com>
2019-10-31 14:52:02 +0800

    2019.10.31(四)

---
+++ UI/Assets/程式碼/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameManager: MonoBehaviour {
+
+    public AudioMixer 音樂管理器;
+
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+}
+
+
