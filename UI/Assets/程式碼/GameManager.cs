28ccfa8fe197d52bc8b40f11d4654fa49bf2a3c1
sloozzz951 <55227248+sloozzz951@users.noreply.github.com>
2019-11-14 15:02:57 +0800

    2019.11.14(四)

--- UI/Assets/程式碼/GameManager.cs
+++ UI/Assets/程式碼/GameManager.cs
@@ -1,17 +1,52 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class GameManager: MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public AudioMixer 音樂管理器;
 
+    public Text loadingText;    
+    public Slider loading;
+
     public void SetVBGM(float value)
     {
-        mixer.SetFloat("VBGM", value);
+        音樂管理器.SetFloat("VBGM", value);
     }
 
-    public void setVSFX(float value)
-        mixer.setFloat("VSFX",value);
+    public void SetVSFX(float value)
+    {
+        音樂管理器.SetFloat("VSFX", value);
     }
+
+    public void Play()
+    {
+        //SceneManager.LoadScene("場景");
+        StartCoroutine(Loading());
+    }
+    private IEnumerator Loading()
+    {
+        //print("測試 1");
+        //yield return new WaitForSeconds(1);
+        //print("測試 2");
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync("場景");
+        ao.allowSceneActivation = false;
+
+        while (ao.isDone == false)
+
+        loadingText.text = ((ao.progress/0.9f) * 100).ToString();
+        loading.value = ao.progress / 0.9f;
+        yield return new WaitForSeconds(0.0001f);
+        
+        if(ao.progress == 0.9f && Input.anyKey)
+        {
+            ao.allowSceneActivation = true;
+        }
+    }
+}
 
 
