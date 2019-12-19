45407eacf0100e9269e7982455b3a294f1247cc9
sloozzz951 <55227248+sloozzz951@users.noreply.github.com>
2019-12-19 15:43:40 +0800

    2019.12.19(四)

---
+++ UI/Assets/程式碼/PauseControl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imagePause;
+    public Sprite spritePause, spritePlay;
+}
